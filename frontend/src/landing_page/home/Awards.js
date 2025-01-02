a26a0687f56d40baed68c422413fa4618030a53f
Numan <9256fa@gmail.com>
2025-01-02 21:58:56 +0530

    Hero component & Awards component: Done

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,10 +1,55 @@
-import React from 'react'
+import React from "react";
 
 function Awards() {
-    return (
-        <div>Awards</div>
-      )
-    
+  return (
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col col-6 ">
+          <img src="/media/images/largestBroker.svg" alt="Award" />
+        </div>
+        <div className="col col-6 p-4">
+          <h1 className="mt-5">Largest stock broker in India</h1>
+          <p className="mb-4 mt-4">
+            2+ million Zerodha clients contribute to over 15% of all retail
+            order volumes in India daily by trading and investing in :
+          </p>
+          <div className="row">
+            <div className="col col-6">
+              <ul className="">
+                <li className="">
+                  <p>Future and Options</p>
+                </li>
+                <li className="">
+                  <p className="">Commodity derivatives</p>
+                </li>
+                <li className="">
+                  <p className="">Currency derivatives</p>
+                </li>
+              </ul>
+            </div>
+            <div className="col col-6">
+              <ul className="">
+                <li className="">
+                  <p>Stock & IPO'</p>
+                </li>
+                <li className="">
+                  <p className="">Direct mutual funds</p>
+                </li>
+                <li className="">
+                  <p className="">Bonds and Govt. Securities</p>
+                </li>
+              </ul>
+            </div>
+            <img
+              className=""
+              src="/media/images/pressLogos.png"
+              alt="Press Logos"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Awards;
--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from "react";
 function Hero() {
-    return ( 
-        <div>Hero</div>
-     );
+  return (
+
+    
+    /*                  <-----------------------   Hero  ------------------------>                     */
+
+
+    <div className="Container p-5 ">
+      <div className="row text-center">
+        <img src="/media/images/homeHero.png" alt="Hero" className="mb-5" />
+        <h1 className="mt-5">Invest in everything</h1>
+        <p>
+          Online platform to invest in stocks, derivatives, mutual funds, ETFs,
+          bonds, and more.
+        </p>
+        <button
+          className="p-2 btn btn-primary fs-6 mb-5 "
+          style={{ width: "15%", margin: "0 auto" }}
+        >
+          Sign Up for free
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
