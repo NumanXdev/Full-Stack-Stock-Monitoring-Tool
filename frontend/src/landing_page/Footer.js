086b2316f7096a0676e5ff6d4375b267491410de
Numan <9256fa@gmail.com>
2025-01-03 22:59:37 +0530

    Footer Component Done

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,7 +1,208 @@
 import React from "react";
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="footer">
+      <div className="container p-5">
+        <div className="row">
+          <div className="col">
+            <img
+              className=""
+              src="/media/images/logo.svg"
+              style={{ width: "50%" }}
+            />
+            <p className="text-muted mt-3 " style={{ fontSize: "12px" }}>
+              &copy; 2010 - 2024, Zerodha Broking Ltd. All rights reserved.
+            </p>
+            <i class="fa-brands fa-x-twitter text-muted"></i>{" "}
+            <i class="fa-brands fa-square-facebook text-muted"></i>
+            <i class="fa-brands fa-instagram text-muted"></i>
+            <i class="fa-brands fa-linkedin-in text-muted"></i>
+            <hr />
+            <i class="fa-brands fa-youtube text-muted"></i>
+            <i class="fa-brands fa-whatsapp text-muted"></i>
+            <i class="fa-brands fa-telegram text-muted"></i>
+          </div>
+          <div className="col ">
+            <p>
+              <b>Company</b>
+            </p>
+            <a href="#" className="text-muted">
+              {" "}
+              About
+            </a>{" "}
+            <br />
+            <a href="#" className="text-muted">
+              Products
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Pricing
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Referral programme
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Careers
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Zerodha.tech
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Press & media
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Zerodha Cares (CSR)
+            </a>
+            <br />
+          </div>
+          <div className="col">
+            <p>
+              <b>Support</b>
+            </p>
+            <a href="#" className="text-muted">
+              {" "}
+              Contact us
+            </a>{" "}
+            <br />
+            <a href="#" className="text-muted">
+              Support portal
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Z-Connect blog
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              List of charges
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Downloads & resources
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Videos
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Market overview
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              How to file a complaint?
+            </a>
+            <br />
+            <a href="#" className="text-muted">
+              Status of your complaints
+            </a>
+            <br />
+          </div>
+          <div className="col">
+            <p>
+              <b>Account</b>
+            </p>
+            <a href="#" className="text-muted">
+              {" "}
+              Open an account
+            </a>{" "}
+            <br />
+            <a href="#" className="text-muted">
+              {" "}
+              Fund Transfer
+            </a>{" "}
+            <br />
+          </div>
+
+          <div className="text-muted para mt-5">
+            <p>
+              Zerodha Broking Ltd. : Member of NSE, BSE​ &​ MCX – SEBI
+              Registration no.: INZ000031633 CDSL/NSDL: Depository services
+              through Zerodha Broking Ltd. – SEBI Registration no.:
+              IN-DP-431-2019 Commodity Trading through Zerodha Commodities Pvt.
+              Ltd. MCX: 46025; NSE-50001 – SEBI Registration no.: INZ000038238
+              Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross,
+              Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase,
+              Bengaluru - 560078, Karnataka, India. For any complaints
+              pertaining to securities broking please write to
+              <a style={{ color: "blue" }} href="#">
+                {" "}
+                complaints@zerodha.com
+              </a>
+              , for DP related to dp@zerodha.com. Please ensure you carefully
+              read the Risk Disclosure Document as prescribed by SEBI | ICF
+            </p>
+
+            <p>
+              Procedure to file a complaint on SEBI SCORES: Register on SCORES
+              portal. Mandatory details for filing complaints on SCORES: Name,
+              PAN, Address, Mobile Number, E-mail ID. Benefits: Effective
+              Communication, Speedy redressal of the grievances
+            </p>
+
+            <p>
+              <a href="#" style={{ color: "blue" }}>
+                Smart Online Dispute Resolution | Grievances Redressal Mechanism
+              </a>
+            </p>
+
+            <p>
+              Investments in securities market are subject to market risks; read
+              all the related documents carefully before investing.
+            </p>
+
+            <p>
+              Attention investors: 1) Stock brokers can accept securities as
+              margins from clients only by way of pledge in the depository
+              system w.e.f September 01, 2020. 2) Update your e-mail and phone
+              number with your stock broker / depository participant and receive
+              OTP directly from depository on your e-mail and/or mobile number
+              to create pledge. 3) Check your securities / MF / bonds in the
+              consolidated account statement issued by NSDL/CDSL every month.
+            </p>
+
+            <p>
+              "Prevent unauthorised transactions in your account. Update your
+              mobile numbers/email IDs with your stock brokers. Receive
+              information of your transactions directly from Exchange on your
+              mobile/email at the end of the day. Issued in the interest of
+              investors. KYC is one time exercise while dealing in securities
+              markets - once KYC is done through a SEBI registered intermediary
+              (broker, DP, Mutual Fund etc.), you need not undergo the same
+              process again when you approach another intermediary." Dear
+              Investor, if you are subscribing to an IPO, there is no need to
+              issue a cheque. Please write the Bank account number and sign the
+              IPO application form to authorize your bank to make payment in
+              case of allotment. In case of non allotment the funds will remain
+              in your bank account. As a business we don't give stock tips, and
+              have not authorized anyone to trade on behalf of others. If you
+              find anyone claiming to be part of Zerodha and offering such
+              services,{" "}
+              <a style={{ color: "blue" }} href="#">
+                please create a ticket here.
+              </a>
+            </p>
+          </div>
+          <div className="bb mt-2 text-muted">
+            <p>NSE</p>
+            <p>BSE</p>
+            <p>MCX</p>
+            <p>Terms & conditions </p>
+            <p>Policies & procedures </p>
+            <p>Privacy policy</p>
+            <p>Disclosure </p>
+            <p>For investor's attention </p>
+            <p>Investor charter </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
