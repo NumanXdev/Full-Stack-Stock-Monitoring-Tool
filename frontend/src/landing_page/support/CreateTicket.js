1df2be3cd9a2c381bb996be2e46aef4d9ec53132
Numan <9256fa@gmail.com>
2025-01-06 12:41:27 +0530

    Ticket Component Done

--- frontend/src/landing_page/support/CreateTicket.js
+++ frontend/src/landing_page/support/CreateTicket.js
@@ -1,8 +1,241 @@
-import React from 'react'
+import React from "react";
 function CreateTicket() {
-    return ( 
-        <div>CreateTicket</div>
-     );
+  return (
+    <div className="container">
+      <div className="row text-muted mt-5">
+        <h5>To create a ticket, select a relevant topic</h5>
+      </div>
+      <div className="row mt-4">
+        <div className="col-4">
+          <a href="#" className="">
+            <h5 className="text-muted">
+              <i className="fa-solid fa-plus-circle"></i>&nbsp; Account Opening
+            </h5>
+          </a>
+          <div className="ms-3 p-3">
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Getting Started
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Online
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Offline
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Charges
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Company, Partnership and HUF
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Non Resident Indian (NRI)
+              </a>
+            </div>
+          </div>
+          {/* Second */} <br />
+          <a href="#" className="">
+            <h5 className="text-muted">
+              <i class="fa-solid fa-wallet"></i>&nbsp; Funds
+            </h5>
+          </a>
+          <div className="ms-3 p-3">
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Getting Started
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Online
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Offline
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Charges
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Company, Partnership and HUF
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Non Resident Indian (NRI)
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="col-4">
+          <a href="#" className="">
+            <h5 className="text-muted">
+              <i class="fa-solid fa-user"></i>&nbsp; Your Zerodha Account
+            </h5>
+          </a>
+          <div className="ms-3 p-3">
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Login credentials
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Your Profile
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Account modification and segment addition
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                CMR & DP ID
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Nomination
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Transfer and conversion of shares
+              </a>
+            </div>
+          </div>
+          {/* Second */} <br />
+          <a href="#" className="">
+            <h5 className="text-muted">
+            <i class="fa-solid fa-circle-user fa-rotate-270"></i>&nbsp; Console
+            </h5>
+          </a>
+          <div className="ms-3 p-3">
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                IPO
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Porfolio
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Funds statement
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Profile
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Reports
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Referral program
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* Third */}
+        <div className="col-4">
+          <a href="#" className="">
+            <h5 className="text-muted">
+              <i class="fa-solid fa-chart-simple"></i>&nbsp; Account Opening
+            </h5>
+          </a>
+          <div className="ms-3 p-3">
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Trading FAQs
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Kite
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Margins
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Product and order types
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Corporate actions
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Kite features
+              </a>
+            </div>
+          </div>
+          {/* Second */} <br />
+          <a href="#" className="">
+            <h5 className="text-muted">
+            <i class="fa-solid fa-coins"></i>&nbsp; Coin
+            </h5>
+          </a>
+          <div className="ms-3 p-3">
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Understanding mutual funds and Coin
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Coin app
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Coin Web
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                Transactions and reports
+              </a>
+            </div>
+            <div className="mt-2">
+              <a style={{ color: "blue", fontSize: "14px" }} href="">
+                National Pension Scheme (NPS)
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CreateTicket;
