0d542ffdc1bd29763d80a2f89331b777cab4d8cc
Numan <9256fa@gmail.com>
2025-01-13 12:54:00 +0530

    Allowing data access form forntend and backend

--- backend/index.js
+++ backend/index.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["http://localhost:3001"],
+    origin: ["http://localhost:3001","http://localhost:3002"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
