eee53681a711a8f9f449c1523bd9ba14306fa3e7
Numan <9256fa@gmail.com>
2025-01-15 01:42:19 +0530

    Added Dropdown feature: Material UI

--- dashboard/src/components/CustomMenu.js
+++ dashboard/src/components/CustomMenu.js
@@ -3,36 +3,50 @@ import { Link } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 import { useCookies } from "react-cookie";
 
-// Material UI
+// Material UI components
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Divider from "@mui/material/Divider";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Tooltip from "@mui/material/Tooltip";
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
 
 const CustomMenu = () => {
   const [cookies] = useCookies(["token"]);
-  const [userData, SetuserData] = useState({ id: "", username: "" });
+  const [userData, setUserData] = useState({ id: "", username: "" });
 
   useEffect(() => {
     if (cookies.token) {
       const decodedToken = jwtDecode(cookies.token);
-      const id = decodedToken.id;
-      const username = decodedToken.username;
-      SetuserData({ id, username });
+      const { id, username } = decodedToken;
+      setUserData({ id, username });
     }
   }, [cookies]);
 
   const [selectedMenu, setSelectedMenu] = useState(0);
-  const [isProfileDropdownOpen, setIsProfileDropdownOpen] = useState(false);
+  const [anchorEl, setAnchorEl] = useState(null);
 
   const handleMenuClick = (index) => {
     setSelectedMenu(index);
   };
 
-  const handleProfileClick = (index) => {
-    setIsProfileDropdownOpen(!isProfileDropdownOpen);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
-  //------------> Avatar Name & Avatr ID
-
-  const Avatar =
-    userData.username.length >= 2
+  // Avatar Name & Avatar ID
+  const avatarName =
+    userData.username && userData.username.length >= 10
       ? userData.username.slice(0, 2).toUpperCase()
       : userData.username.slice(0, 1);
   const userId = userData.id.slice(0, 3);
@@ -42,7 +56,7 @@ const CustomMenu = () => {
 
   return (
     <div className="menu-container">
-      <img src="logo.png" style={{ width: "50px" }} />
+      <img src="logo.png" style={{ width: "50px" }} alt="Logo" />
       <div className="menus">
         <ul>
           <li>
@@ -113,10 +127,76 @@ const CustomMenu = () => {
           </li>
         </ul>
         <hr />
-        <div className="profile" onClick={handleProfileClick}>
+         {/* <div className="profile" onClick={handleProfileClick}>
           <div className="avatar">{Avatar}</div>
           <p className="username">{userId}</p>
-        </div>
+        </div> */}
+        <React.Fragment>
+          <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+       
+            <Tooltip title="Your Account">
+              <IconButton
+                onClick={handleClick}
+                size="small"
+                sx={{ ml: 2 }}
+                aria-controls={open ? "account-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              >
+                <Avatar  sx={{ width: 32, height: 32,fontSize:"0.8rem",backgroundColor: 'primary.main' }}>{avatarName}</Avatar>
+              </IconButton>
+            </Tooltip>
+          </Box>
+          <Menu
+            anchorEl={anchorEl}
+            id="account-menu"
+            open={open}
+            onClose={handleClose}
+            onClick={handleClose}
+            PaperProps={{
+              elevation: 0,
+              sx: {
+                overflow: "visible",
+                filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                mt: 1.5,
+                "& .MuiAvatar-root": {
+                  width: 32,
+                  height: 32,
+                  ml: -0.5,
+                  mr: 1,
+                },
+                "&::before": {
+                  content: '""',
+                  display: "block",
+                  position: "absolute",
+                  top: 0,
+                  right: 14,
+                  width: 10,
+                  height: 10,
+                  bgcolor: "background.paper",
+                  transform: "translateY(-50%) rotate(45deg)",
+                  zIndex: 0,
+                },
+              },
+            }}
+            transformOrigin={{ horizontal: "right", vertical: "top" }}
+            anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+          >
+            <MenuItem onClick={handleClose}>
+              <Avatar /> {userData.username}
+            </MenuItem>
+           
+            <Divider /> 
+            
+            
+            <MenuItem onClick={handleClose}>
+              <ListItemIcon>
+                <Logout fontSize="small" />
+              </ListItemIcon>
+              Logout
+            </MenuItem>
+          </Menu>
+        </React.Fragment>
       </div>
     </div>
   );
