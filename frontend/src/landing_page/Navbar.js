f4e4cfff01c1e6f530a185458877f22fc1bc18d5
Numan <9256fa@gmail.com>
2025-01-16 22:57:31 +0530

    fixing bugs and errors p-7

--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -59,8 +59,10 @@ function Navbar() {
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="full-stack-stock-monitoring-tool-r7oc.vercel.app
-/dashboard">
+              <Link
+                className="nav-link"
+                to="full-stack-stock-monitoring-tool-r7oc.vercel.app/dashboard"
+              >
                 Dashboard
               </Link>
             </li>
